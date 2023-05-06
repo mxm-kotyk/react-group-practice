dbadfe7c62807773705be94bbae7a9a64dc8d02b
Дмитрий Хоменко <dimkakhomenko@gmail.com>
2023-05-06 13:04:26 +0300

    done

--- src/components/CryptoHistory/CryptoHistory.jsx
+++ src/components/CryptoHistory/CryptoHistory.jsx
@@ -1,5 +1,25 @@
 import { BaseTable, THead, Th, Tr, Td } from './CryptoHistory.styled';
+import { timeCrypto } from '../../utils/date-fns'
+export const CryptoHistory = ({ transactions }) => {
+  return <BaseTable>
+    <THead>
+      <tr>
+        <Th>№</Th>
+        <Th>PRICE</Th>
+        <Th>AMOUNT</Th>
+        <Th>DATE</Th>
+      </tr>
+    </THead>
 
-export const CryptoHistory = () => {
-  return <div>CryptoHistory</div>;
+    <tbody>
+      {transactions.map(({ id, price, amount, date }, i) => {
+        return <Tr key={id}>
+          <Td>{i + 1}</Td>
+          <Td>{price}</Td>
+          <Td>{amount}</Td>
+          <Td>{timeCrypto(date)}</Td>
+        </Tr>
+      })}
+    </tbody>
+  </BaseTable>;
 };
