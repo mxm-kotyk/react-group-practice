226021a3e807b4ae40f93e64b1b53fd995d1623f
VitalyOvsiyenko <ovsiyenko80@gmail.com>
2023-05-06 12:48:52 +0300

    Task 3

--- src/components/ForbesList/ForbesList.jsx
+++ src/components/ForbesList/ForbesList.jsx
@@ -8,6 +8,19 @@ import {
   LeaderBoardProfiles,
 } from './ForbesList.styled';
 
-export const ForbesList = () => {
-  return <div>ForbesList</div>;
+export const ForbesList = ({forbes}) => {
+  return <LeaderBoard>
+  <BoardHeader>
+    <BoardTitle>
+      <TitleTop>Forbes</TitleTop>
+      <TitleBottom>Leader board</TitleBottom>
+    </BoardTitle>
+  </BoardHeader>
+
+  <LeaderBoardProfiles>
+      {forbes.map(({ avatar, name, capital, isIncrease, id}) => {
+        return (<ForbesListItem avatar={avatar} name={name} capital={capital} isIncrease ={isIncrease} key={id}
+      />)})} 
+  </LeaderBoardProfiles>
+</LeaderBoard>;
 };
